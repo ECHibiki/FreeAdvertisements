02e33e04a6f4175c580899984a68c21fb691bc60
ECVerniy <ecverniy@gmail.com>
2020-01-30 22:43:38 -0500

    Creation of feature tests for login

--- readme.md
+++ readme.md
@@ -4,6 +4,5 @@ Done with Test Driven Development on both React and Laravel PHP, this is a simpl
 Uses Larvel, jwt-auth, React, Docker(laradock), Twig, PHPUnit, React Testing Library with Jest-DOM and Bootstrap.
 <br/><br/>
 Run on linux with 
-```docker-compose up -d mysql nginx
-docker-compose exec -u laradock workspace bash``` sudo prefix as required. Stop on existing ports.<br/>
+```docker-compose up -d mysql nginx && docker-compose exec -u laradock workspace bash``` sudo prefix as required. Stop on existing ports.<br/>
 Run PHP tests with ```phpunit```<br/>
--- tests/Feature/UserAccessTests.php
+++ tests/Feature/UserAccessTests.php
@@ -8,6 +8,9 @@ use Tests\TestCase;
 
 class UserAccessTests extends TestCase
 {
+
+    use RefershDatabase;
+
     /**
      * A basic feature test example.
      *
@@ -20,12 +23,189 @@ class UserAccessTests extends TestCase
         $response->assertStatus(200);
     }
 
-    public function a_user_is_created(){}
+    //jwt token generation and user session
+    public function a_user_is_sucessfuly_created(){
+	$response = $this->call('POST', 'user/register.php', ['name'=>'test', 'password'=>'hashedtestpass', 'email'=>'adtest10080@gmail.com']);
+	$response
+		->assertStatus(200)
+		->assertJson([
+                'created' => '1',
+            ]);
+	
+	//check db for user
+	$this->assertDatabaseHas('users', [
+        	'email' => 'adtest10080@gmail.com', 'password'=>'hashedtestpass', 'name'=>'test'
+    	    ]);
+    }   
 
-    public function a_user_logs_in(){}
+    public function a_duplicate_user_is_created(){
+    	$response = $this->call('POST', 'user/register.php', ['name'=>'test', 'password'=>'hashedtestpass', 'email'=>'adtest10080@gmail.com']);
+	$response
+		->assertStatus(400)
+		->assertJson([
+                'created' => "0",
+            ]);
 
-    public function a_user_logs_out(){}
+	//check db for user
+	$this->assertDatabaseMissing('users', [
+            'email' => 'adtest10080@gmail.com', 'password'=>'hashedtestpass', 'name'=>'test'
+	]);
+    }
 
-    public function a_user_is_deleted(){}
+    public function email_field_does_not_exist_and_user_is_created(){
+        $response = $this->call('POST', 'user/register.php', ['name'=>'test', 'password'=>'hashedtestpass', 'email'=>'']);
+	$response
+		->assertStatus(400)
+		->assertJson([
+                'created' => "-3",
+            ]);
+
+	//check db for user
+	$this->assertDatabaseMissing('users', [
+            'password'=>'hashedtestpass', 'name'=>'test'
+	]);
+    }
+
+    public function email_field_does_not_exist_and_user_is_created(){
+        $response = $this->call('POST', 'user/register.php', ['name'=>'test', 'password'=>'', 'email'=>'']);
+	$response
+		->assertStatus(400)
+		->assertJson([
+                'created' => "-2",
+            ]);
+
+	//check db for user
+	$this->assertDatabaseMissing('users', [
+            'email' => 'adtest10080@gmail.com', 'name'=>'test'
+	]);
+    }
+    public function name_field_does_not_exist_and_user_is_created(){
+        $response = $this->call('POST', 'user/register.php', ['name'=>'', 'password'=>'hashedtestpass', 'email'=>'adtest10080@gmail.com']);
+	$response
+		->assertStatus(400)
+		->assertJson([
+                'created' => "-1",
+            ]);
+
+	//check db for user
+	$this->assertDatabaseMissing('users', [
+            'email' => 'adtest10080@gmail.com', 'password'=>'hashedtestpass'
+	]);
+    }
+
+
+    public function an_existing_user_logs_in(){
+	//redundant but easy    
+	$response = $this->call('POST', 'user/register.php', ['name'=>'test', 'password'=>'hashedtestpass', 'email'=>'adtest10080@gmail.com']);
+	$response
+		->assertStatus(200)
+		->assertJson([
+                'created' => '1',
+            ]);
+
+	$response = $this->call('POST', 'user/login.php', ['name'=>'test', 'password'=>'hashedtestpass']);
+        $response
+		->assertStatus(200)
+		->assertJsonCount(1, 'jwt');
+
+        $token = $this->response->getOriginalContent()->getData();
+    	//evaluate token as needed
+    }
+
+    public function a_non_existing_user_tries_to_log_in(){
+  	//redundant but easy    
+	$response = $this->call('POST', 'user/register.php', ['name'=>'test', 'password'=>'hashedtestpass', 'email'=>'adtest10080@gmail.com']);
+	$response
+		->assertStatus(200)
+		->assertJson([
+                'created' => '1',
+            ]);
+
+	$response = $this->call('POST', 'user/login.php', ['name'=>'test', 'password'=>'hashedtestpass']);
+        $response
+		->assertStatus(401)
+		->assertJson('log'=>'-1'); //login error
+
+    }
+
+    public function an_existing_user_logs_out(){
+        //redundant but easy    
+	$response = $this->call('POST', 'user/register.php', ['name'=>'test', 'password'=>'hashedtestpass', 'email'=>'adtest10080@gmail.com']);
+	$response
+		->assertStatus(200)
+		->assertJson([
+                'created' => '1',
+            ]);
+
+	$response = $this->call('POST', 'user/login.php', ['name'=>'test', 'password'=>'hashedtestpass']);
+        $response
+		->assertStatus(200)
+		->assertJsonCount(1, 'jwt');
+
+        $token = $this->response->getOriginalContent()->getData();
+    	//evaluate token as needed
+	$response = $this->call('POST', 'user/logout.php', ['jwt'=>$token]);
+        $response
+		->assertStatus(200)
+		->assertJson('log'=>'1'); //success
+	$response = $this->call('POST', 'user/logout.php', ['jwt'=>$token]);
+        $response
+		->assertStatus(401)
+		->assertJson('log'=>'-2'); //invalid token error
+    }
+
+    public function a_non_existing_user_logs_out(){
+        $token = "garbage~.^data.inzide";
+    	//evaluate token as needed
+	$response = $this->call('POST', 'user/logout.php', ['jwt'=>$token]);
+        $response
+		->assertStatus(400)
+		->assertJson('log'=>'-2'); //invalid token error
+
+    }
+
+    public function a_legitimate_token_is_used_to_access_private_page(){
+        //redundant but easy    
+	$response = $this->call('POST', 'user/register.php', ['name'=>'test', 'password'=>'hashedtestpass', 'email'=>'adtest10080@gmail.com']);
+	$response
+		->assertStatus(200)
+		->assertJson([
+                'created' => '1',
+            ]);
+
+	$response = $this->call('POST', 'user/login.php', ['name'=>'test', 'password'=>'hashedtestpass']);
+        $response
+		->assertStatus(200)
+		->assertJsonCount(1, 'jwt');
+
+        $token = $this->response->getOriginalContent()->getData();
+    	//evaluate token as needed
+        $response = $this->call('POST', 'user/user-data.php', ['jwt'=>$token]);
+	$response
+		->assertStatus(200);
+    }
+
+    public function a_token_is_expired_when_used_on_private_page(){
+            //redundant but easy    
+	$response = $this->call('POST', 'user/register.php', ['name'=>'test', 'password'=>'hashedtestpass', 'email'=>'adtest10080@gmail.com']);
+	$response
+		->assertStatus(200)
+		->assertJson([
+                'created' => '1',
+            ]);
+
+	$response = $this->call('POST', 'user/login.php', ['name'=>'test', 'password'=>'hashedtestpass']);
+        $response
+		->assertStatus(200)
+		->assertJsonCount(1, 'jwt');
+
+        $token = $this->response->getOriginalContent()->getData();
+    	//evaluate token as needed
+        $response = $this->call('POST', 'user/user-data.php', ['jwt'=>$token]);
+	$response
+		->assertStatus(401)
+		->assertJson('access'=>'-1');
+
+    }
 
 }
