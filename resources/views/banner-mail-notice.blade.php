f48af4cd41b18de8438c8e9ec1c60e26c7f052e7
ECHibiki <ecverniy@gmail.com>
2020-03-12 04:15:49 -0400

    Update banner-mail-notice.blade.php

--- resources/views/banner-mail-notice.blade.php
+++ resources/views/banner-mail-notice.blade.php
@@ -1,6 +1,6 @@
 <h2>New Banner @ {{ $time }}</h2>
 <p style="color:green">Name:  {{ $name }}</p><br/>
-<p style="color:cyan">URL: {{$url}}</p>
+<p style="color:blue">URL: {{$url}}</p>
 @if ($err)
 <hr/>
 <p>Previous Send Errors: <pre>{{ $err }}</pre>
