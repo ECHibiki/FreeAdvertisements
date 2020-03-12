4034df5738f1b3830e8f3188a4c88644624be29a
ECHibiki <ecverniy@gmail.com>
2020-03-12 02:52:40 -0400

    Update database.php

--- config/database.php
+++ config/database.php
@@ -66,7 +66,7 @@ return [
               'driver' => 'mysql',
               'host' => env('DB_TEST_HOST', 'localhost'),
               'database' => env('DB_TEST_DATABASE', 'testing'),
-              'username' => env('DB_TEST_USERNAME', 'test'),
+              'username' => env('DB_TEST_USERNAME', 'root'),
               'password' => env('DB_TEST_PASSWORD', 'secret'),
               'charset' => 'utf8',
               'collation' => 'utf8_unicode_ci',
