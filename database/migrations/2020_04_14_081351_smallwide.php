26765931843c5204b04df7c0a39972d6aed4af37
ECHibiki <ecverniy@gmail.com>
2020-04-14 06:09:19 -0400

    add alternate size(fix migration)

--- database/migrations/2020_04_14_081351_smallwide.php
+++ database/migrations/2020_04_14_081351_smallwide.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Smallbig extends Migration
+class Smallwide extends Migration
 {
     /**
      * Run the migrations.
